e3dd4425db5a84141d30b0f71c78fdaf45599e9f
gamblinflanagan <gamblinflanagan97@gmail.com>
2021-07-14 15:25:04 -0400

    fix errors in paypal.js

--- src/static/components/paypal.js
+++ src/static/components/paypal.js
@@ -10,30 +10,32 @@ const PayPal = ()  => {
     const paypal = useRef();
 
     useEffect(() => {
-        window.paypal.Buttons({
+        window.paypal
+          .Buttons({
             createOrder: (data, actions, err) => {
-                return actions.order.create({
-                    intent: "CAPTURE",
-                    purchase_inits: [
-                        {
-                            description: "Donation",
-                            amount: {
-                                currency_code: "USD",
-                                value: donationAmount,
-                            },
-                        },
-                    ], 
-                })
+              return actions.order.create({
+                intent: "CAPTURE",
+                purchase_units: [
+                  {
+                    description: "Donation",
+                    amount: {
+                      currency_code: "USD",
+                      value: donationAmount,
+                    },
+                  },
+                ],
+              });
             },
             onApprove: async (data, actions) => {
-                const order = await (actions.order.capture())
-                console.log(order)
+              const order = await actions.order.capture();
+              console.log(order);
             },
             onError: (err) => {
-                console.log(err)
-            }
-        }).render(paypal.current)
-    }, [])
+              console.log(err);
+            },
+          })
+          .render(paypal.current);
+      }, []);
 
     return (
         <div ref={paypal}></div>
