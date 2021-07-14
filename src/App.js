3e3278e29b7fb8f00c93e300bfccca252f362fc9
gamblinflanagan <gamblinflanagan97@gmail.com>
2021-07-14 11:31:24 -0400

    clean up App.js, create contingency in header.js

--- src/App.js
+++ src/App.js
@@ -11,32 +11,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-/*
-import logo from './LPI logo (2).jpg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Nj Homeless</h1>
-        <a
-          className="App-link"
-          href="https://www.paypal.me/lpinj"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Donate Today
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-*/
