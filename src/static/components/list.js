630a5117d985ecee2519e6f4fa00eb2ee8f02107
gamblinflanagan <gamblinflanagan97@gmail.com>
2021-07-16 07:12:24 -0400

    create list.js file move list code from header.js

---
+++ src/static/components/list.js
@@ -0,0 +1,43 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { Button } from 'react-bootstrap';
+
+import '../style/App.css';
+
+
+const initialInfo = [
+    {
+      date: "05/18/1998",
+      amount: 10
+    }
+  ];
+  const [info, setInfo] = useState(initialInfo);
+
+  function GetPaymentData() {
+    var inDate = "02/28/1997" 
+    var inAmount = 11
+    info.push({ date: inDate, amount: inAmount });
+    setInfo(() => info.map((item) => item));
+    return info;
+  }
+
+
+const List = ()  => {
+    
+    return (
+      <div className="List">
+        <span>
+          <ul>
+            {info.map((item) => (
+              <li key={item.id}>
+                <label>
+                  {item.date} ${item.amount}
+                </label>
+              </li>
+            ))}
+          </ul>
+        </span>
+      </div>
+    );
+}
+    
+export default List;
