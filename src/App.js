54dfd2c43b2a1705aa0f9acec30932a9d693a471
gamblinflanagan <gamblinflanagan97@gmail.com>
2021-07-27 09:48:36 -0400

    fix decimal point input error, add copyright

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App() {
   return (
     <div>
       <Header />
+      <p className="copyright">
+        Copyright 2021
+        Developed by <a href="https://gamblinflanagan.herokuapp.com" target="_blank">Joseph Flanagan</a>&nbsp;
+        Designed by <a href="https://www.allisoneglow.com" target="_blank">Allsion Eglow</a>
+      </p>
     </div>
   )
 }
