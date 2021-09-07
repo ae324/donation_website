0f865e7059fdfb605bdcc68c355742a0482dba54
gamblinflanagan <gamblinflanagan97@gmail.com>
2021-09-07 13:56:06 -0400

    make donation table scrollable

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 import Header from "./static/components/header";
 
@@ -6,9 +7,15 @@ function App() {
   return (
     <div>
       <Header />
-      <footer className="copyright" style={{textAlign:"center"}}>
-          &#169; 2021 Developed & Designer by <a href="https://gamblinflanagan-dc7f7.web.app" target="_blank">Joseph Flanagan</a> & <a href="https://www.allisoneglow.com" target="_blank">Allison Eglow</a>
-      </footer>
+      <Container className="CopyrightContainer">
+        <Row>
+          <Col md>
+            <footer className="copyright">
+                &#169; 2021 Developed & Designer by <a href="https://gamblinflanagan-dc7f7.web.app" target="_blank">Joseph Flanagan</a> & <a href="https://www.allisoneglow.com" target="_blank">Allison Eglow</a>
+            </footer>
+          </Col>
+        </Row>
+        </Container>
     </div>
   )
 }
